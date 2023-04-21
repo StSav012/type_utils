88a27eb22a690e759708268d1ac24bcc0cd7e327
stsav012 <stsav012@gmail.com>
2023-04-21 14:41:39 +0300

    Hardcode some corrections of the docstrings

--- type_qt.py
+++ type_qt.py
@@ -68,6 +68,8 @@ def ensure_arg_names(filename: Path, line: str) -> str:
         },
         'QListWidgetItem.py': {
             # PySide6
+            '__init__(self, icon: Union[PySide6.QtGui.QIcon, PySide6.QtGui.QPixmap], text: str, listview: Optional[PySide6.QtWidgets.QListWidget] = None, type: int = <ItemType.Type: 0>) -> None':
+                '__init__(self, icon: Union[PySide6.QtGui.QIcon, PySide6.QtGui.QPixmap], text: str, listview: Optional[PySide6.QtWidgets.QListWidget] = None, type: PySide6.QtWidgets.QListWidgetItem.ItemType = PySide6.QtWidgets.QListWidgetItem.ItemType.Type) -> None',
             '__init__(self, listview: Optional[PySide6.QtWidgets.QListWidget] = None, type: int = <ItemType.Type: 0>) -> None':
                 '__init__(self, listview: Optional[PySide6.QtWidgets.QListWidget] = None, type: PySide6.QtWidgets.QListWidgetItem.ItemType = PySide6.QtWidgets.QListWidgetItem.ItemType.Type) -> None',
             '__init__(self, text: str, listview: Optional[PySide6.QtWidgets.QListWidget] = None, type: int = <ItemType.Type: 0>) -> None':
